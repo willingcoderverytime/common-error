396777b2939b3c461f9c3143df606c90385760be
249639015@qq.conm <249639015@qq.conm>
2024-09-20 21:24:07 +0800

    --  0.1.0 remove test build

--- common-error-test/src/_common_error.rs
+++
@@ -1,5 +0,0 @@
-extern crate common_error; 
- 
-
-# [doc = "zheshi yige xiaoxiao de wenti "] pub struct SYS00001 ; impl SYS00001 { pub fn throw (code : & str) -> (common_error :: CommonError , String) { let msg = "sys error " . to_string () + code + "" ; let common_error = common_error :: CommonError { code : "SYS00001" . to_string () , message : msg . clone () } ; (common_error , msg) } }
-# [doc = "true"] pub struct SYS00002 ; impl SYS00002 { pub fn throw (file_name : & str , msg : & str) -> (common_error :: CommonError , String) { let msg = "file " . to_string () + file_name + " open file error info : " + msg + "" ; let common_error = common_error :: CommonError { code : "SYS00002" . to_string () , message : msg . clone () } ; (common_error , msg) } }
--- common-error-test/src/main.rs
+++
@@ -1,7 +0,0 @@
-use crate::_common_error::SYS00001;
-
-mod _common_error;
-fn main() {
-    let _throw1 = SYS00001::throw("cc");
-    println!("Hello, world!");
-}
